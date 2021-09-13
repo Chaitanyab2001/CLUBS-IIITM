374a813db77813438eaa2de97250f8733bb0ad16
Ninad Kalanke <ninadkalanke@gmail>
2021-09-13 22:33:05 +0530

    get, put, post, delete on /club and /event are implimented and are test by Ninad and Jay OK

--- controllers/clubs.js
+++ controllers/clubs.js
@@ -13,7 +13,7 @@ export const getClubs = async (req,res) => {
 
 };
 
-export const createClub = async (req,res) => {
+export const postClub = async (req,res) => {
 
     const body = req.body;
     const newClub = new clubModel(body);
@@ -21,11 +21,44 @@ export const createClub = async (req,res) => {
     try {
         await newClub.save();
         res.setHeader("ContentType", "application/json");
-        res.status(201).json(newClub);
+        res.status(200).json(newClub);
         
     } catch (error) {
         res.setHeader("ContentType", "application/json");
         res.status(error.status).json({ message: error.message });        
     }
 
+};
+
+
+export const putClub = async (req,res) => {
+
+    const body = req.body;
+
+    try {
+        await clubModel.updateOne({ _id: body._id }, req.body);
+        res.setHeader("ContentType", "application/json");
+        res.status(200).json(await clubModel.findOne(body));
+
+    } catch (error) {
+        res.setHeader("ContentType", "application/json");
+        res.status(error.status).json({ message: error.message });     
+    }
+
+};
+
+export const delClub = async (req,res) => {
+
+    const body = req.body;
+
+    try {
+        await clubModel.deleteOne(body);
+        res.setHeader("ContentType", "application/json");
+        res.status(200).json(body);
+        
+    } catch (error) {
+        res.setHeader("ContentType", "application/json");
+        res.status(error.status).json({ message: error.message });    
+
+    }
 };
--- routes/clubs.js
+++ routes/clubs.js
@@ -1,10 +1,12 @@
 import express from "express";
 
-import { getClubs, createClub } from "../controllers/clubs.js";
+import { getClubs, postClub, putClub, delClub } from "../controllers/clubs.js";
 
 const router = express.Router();
 
 router.get("/", getClubs);
-router.post("/", createClub);
+router.post("/", postClub);
+router.put("/", putClub);
+router.delete("/", delClub);
 
 export default router;
