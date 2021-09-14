9d1afad95b4f203df37283958a291db3c27dc946
Akshat Gupta <akshatg450@gmail.com>
2021-09-14 11:11:56 +0530

    Second commit

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -8,13 +9,16 @@ import { username, password } from "./credentials.js";
 import clubRouter from "./routes/clubs.js";
 import eventRouter from "./routes/events.js";
 
-const findOrCreate = require('mongoose-findorcreate')
+import findOrCreate from "mongoose-findorcreate";
 
-const session = require("express-session");
-const passport = require("passport");
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+import  session from "express-session";
+import passport from "passport";
+
+import Googlepassport from "passport-google-oauth20";
+const GoogleStrategy = Googlepassport.Strategy;
 
 const app = express();
+app.use(cors());
 
 app.use(express.json({ limit: "30mb", extended: true })) 
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -28,7 +32,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors());
+
 
 app.use("/club", clubRouter);
 app.use("/event", eventRouter);
@@ -39,7 +43,7 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`The server is running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
-    mongoose.set('useCreateIndex', true);
+    // mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
         googleId: String
@@ -49,7 +53,7 @@ userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
-passport.use(User.createStrategy());
+// passport.use(User.createStrategy());
  
 // use static serialize and deserialize of model for passport session support
 passport.serializeUser(function(user, done) {
@@ -65,7 +69,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID ,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/",
+    callbackURL: "http://localhost:5000/auth/google/club",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -79,7 +83,7 @@ passport.use(new GoogleStrategy({
 app.get("/auth/google",
   passport.authenticate('google', { scope: ["profile"] }));
 
-app.get("/auth/google/", 
+app.get("/auth/google/club", 
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
