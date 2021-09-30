076921dcd4536cf5cd9a4b4b2fa513590d845a91
Ninad Kalanke <ninadkalanke@gmail>
2021-09-30 16:53:26 +0530

    formrendering routes added

--- routes/club.js
+++ routes/club.js
@@ -1,4 +1,5 @@
 import express from "express";
+import clubModel from "../models/clubs.js";
 import { getClub, postClub } from "../controllers/clubs.js";
 import { postEvent } from "../controllers/events.js";
 import { getClubApprovals, postApproval } from "../controllers/approvals.js";
@@ -49,6 +50,69 @@ router.post("/", async function(req,res,next) {
         res.status(200).json(club);
     }
 
+});
+
+router.get("/:clubId/event", async function(req,res,next){
+
+    if(req.session.passport === undefined)
+    {
+        var err = new Error("You are not logged in.");
+        err.status = 400;
+        res.status(err.status).send(err.message);
+    }
+    
+    const { clubId } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(clubId))
+    {
+        var err = new Error("The Club doesn't exsist.");
+        err.status = 406;
+        res.status(err.status).send(err.message);
+    }
+
+    var club;
+    
+    try {
+        club = await clubModel.findOne({ _id: clubId });
+        
+    } catch (error) {
+        error.message = "Unable to connect with database.";
+        res.status(error.status).send(error.message);          
+    }
+
+    if(club === null)
+    {
+        var err = new Error("The Club doesn't exsist.");
+        err.status = 406;
+        res.status(error.status).send(error.message); 
+    }
+
+    if(club.residentid != req.session.passport.user)
+    {
+        var err = new Error("You are not president of club.");
+        err.status = 400;
+        res.status(error.status).send(error.message); 
+    }
+
+    res.status(200).send("The event creation form will render here.")
+});
+
+router.put("/:eventId", async function(req,res,next) {
+
+    const event = await putEvent(req,res);
+
+    if(Object.prototype.toString.call(event) === "[object Error]")
+    {
+        if((event.status) < 500)
+        res.status(event.status).send(event.message);
+        else
+        next(event.message);
+    }
+    else
+    {
+        res.setHeader("ContentType", "application/json");
+        res.status(200).send("The event is updated successfully.");
+    }
 });
 
 router.post("/:clubId/event", async function(req,res,next) {
--- routes/event.js
+++ routes/event.js
@@ -1,4 +1,5 @@
 import express from "express";
+import eventModel from "../models/events.js";
 import { getEvent, putEvent, delEvent } from "../controllers/events.js";
 
 const router = express.Router();
@@ -19,6 +20,54 @@ router.get("/:eventId", async function(req,res,next) {
         res.setHeader("ContentType", "application/json");
         res.status(200).json(event);
     }
+});
+
+router.get("/:eventId/edit", async function(req,res,next){
+
+    // Already posted data redenering remaining
+    
+    if(req.session.passport === undefined)
+    {
+        var err = new Error("You are not logged in.");
+        err.status = 400;
+        res.status(err.status).send(err.message);
+    }
+    
+    const { eventId } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(eventId))
+    {
+        var err = new Error("The Event doesn't exsist.");
+        err.status = 406;
+        res.status(err.status).send(err.message);
+    }
+
+    var event;
+    
+    try {
+        event = await eventModel.findOne({ _id: eventId })
+                                .populate("clubid", "presidentid");
+        
+    } catch (error) {
+        error.message = "Unable to connect with database.";
+        res.status(error.status).send(error.message);          
+    }
+
+    if(event === null)
+    {
+        var err = new Error("The Event doesn't exsist.");
+        err.status = 406;
+        res.status(error.status).send(error.message); 
+    }
+
+    if(event.clubid.presidentid != req.session.passport.user)
+    {
+        var err = new Error("You are not president of club.");
+        err.status = 400;
+        res.status(error.status).send(error.message); 
+    }
+
+    res.status(200).send("The event edit form will render here.")
 });
 
 router.put("/:eventId", async function(req,res,next) {
